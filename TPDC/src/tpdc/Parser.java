0aaf264036ff8bff28be2053f5d5e93b94bcd8ea
arurke@gmail.com <arurke@gmail.com@93df2a13-8f89-6fd1-c5de-bfba8da473a2>
2012-01-16 08:35:36 +0000

    underveis

--- TPDC/src/tpdc/Parser.java
+++ TPDC/src/tpdc/Parser.java
@@ -33,17 +33,125 @@ public class Parser {
 	
 	public String getSipDomain(){
 		searchString = "SIP Domains Domain 1 Name: ";
-		String strLine;
+		return findValue();
+	}
+	
+	public String getIPaddress(){
+		searchString = "Ethernet 1 IP V4 Address: ";
+		return findValue();
+	}
+	
+	public String getSubnetmask(){
+		searchString = "Ethernet 1 IP V4 SubnetMask: ";
+		return findValue();
+	}
+	
+	public String getGateway(){
+		searchString = "IP Gateway: ";
+		return findValue();
+	}
+	
+	public String getHostname(){
+		searchString = "IP DNS Hostname: ";
+		return findValue();
+	}
+	
+	public String getDomain(){
+		searchString = "DNS Domain Name: ";
+		return findValue();
+	}
+	
+	public String getNTPserver(){
+		searchString = "NTP Address: ";
+		return findValue();
+	}
+	
+	public String getTMSaddress(){
+		searchString = "ExternalManager Address: ";
+		return findValue();
+	}
+	
+	public String getHostname(){
+		searchString = "SystemUnit Name: ";
+		return findValue();
+	}
+	
+	public String getRegRestriction(){
+		searchString = "Registration RestrictionPolicy Mode: ";
+		return findValue();
+	}
+	
+	public String getLoopDetection(){
+		searchString = "Call Loop Detection Mode: ";
+		return findValue();
+	}
+	
+	public String getRoutedMode(){
+		searchString = "Call Routed Mode: ";
+		return findValue();
+	}
+	
+	public String getCallToUnknownIP(){
+		searchString = "Call Services CallsToUnknownIPAddresses: ";
+		return findValue();
+	}
+	
+	public String getH323mode(){
+		searchString = "H323 Mode: ";
+		return findValue();
+	}
+	
+	public String getSIPmode(){
+		searchString = "SIP Mode: ";
+		return findValue();
+	}
+	
+	public String getInterworkingMode(){
+		searchString = "Interworking Mode: ";
+		return findValue();
+	}
+	
+	public String getHostname(){
+		searchString = "SystemUnit Name: ";
+		return findValue();
+	}
+	
+	public String getHostname(){
+		searchString = "SystemUnit Name: ";
+		return findValue();
+	}
+	
+	public String getHostname(){
+		searchString = "SystemUnit Name: ";
+		return findValue();
+	}
+	
+	public String getHostname(){
+		searchString = "SystemUnit Name: ";
+		return findValue();
+	}
+	
+	public String getHostname(){
+		searchString = "SystemUnit Name: ";
+		return findValue();
+	}
+	
+	public String getHostname(){
+		searchString = "SystemUnit Name: ";
+		return findValue();
+	}
+	
+	private String findValue(){
 		String value = "";
 		while (xconfScanner.hasNextLine()){
-			strLine = xconfScanner.nextLine();
-			int test = strLine.indexOf(searchString);
-			if (test != -1){
-				value = strLine.substring(test+searchString.length(),strLine.length());
+			String strLine = xconfScanner.nextLine();
+			if (strLine.indexOf(searchString) != -1){
+				value = strLine.substring(strLine.indexOf(searchString)+searchString.length(),strLine.length());
 			}
 		}
 		return validValue(value);
 	}
+	
 	private String validValue(String value){
 		if (value != "")return value;
 		else return "N/A";
